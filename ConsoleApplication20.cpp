3a6d2f30226d72889224f7af5afe369b0c428eab
Святослав Харченко <svyatoslav71tula@gmail.com>
2023-01-05 17:05:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication20.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+void counter() {
+    static int count = 0;
+    std::cout << count++;
+}
+
+int main() {
+    for (int i = 0; i < 10; ++i) {
+        counter();
+    }
+    return 0;
+}
